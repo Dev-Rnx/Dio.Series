3c15cf1b67bcbb4a9a8fdfeed93876ef6c30e41d
Rennan Alves <rennan_areas@hotmail.com>
2022-01-20 15:33:29 -0300

    Update Aula02

--- Dio.Series/Program.cs
+++ Dio.Series/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace Dio.Series
+﻿namespace Dio.Series
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
